d17bbf595ab4b6c4405f911e12954a843ec513d3
AJITH KUMAR M <89124776+ajitharunai@users.noreply.github.com>
2023-02-13 16:20:34 +0530

    Create Fizz Buzz.py

---
+++ Fizz Buzz.py
@@ -0,0 +1,11 @@
+#Fizz Buzz: Write a program that prints the numbers from 1 to 100, but for multiples of 3
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
